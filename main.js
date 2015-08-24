c47f343302f017080e58c5f9995c6514c9847126
ajchan11 <ajchan11@gmail.com>
2015-08-24 15:11:57 -0700

    git workflows

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-var boxes = $(".picX")
+var boxes = $(".picX");
+var active = $(".active");
+var display= 0;
+var score = 0;
+$(".score").html(display);
+console.log(display)
+$(".score").html("Score:" + display);
 
-var time = Math.floor(Math.random() * 1000);
+// var time = (Math.floor(Math.random() * 1000));
+var time = 1000;
 console.log(time)
 
 setInterval(swap, time)
@@ -9,25 +16,45 @@ var pick =0 ;
 var random = $(boxes.get(Math.floor(Math.random() * boxes.length)));
 console.log(random.hasClass("picX"))
 
-function swap () {
-	if(random.hasClass("active")){
+
+
+function swap () 
+{
+	if(random.hasClass("active"))
+		{
 		console.log("has class active")
-		random.removeClass("active")
-		time = Math.floor(Math.random() * 1000);
+		random.removeClass("active").animate
+			({
+				opacity:1
+			})
+		time = (Math.floor(Math.random() * 1000));
 		console.log(time)
 		random = $(boxes.get(Math.floor(Math.random() * boxes.length)));
-console.log(random.hasClass("picX"))
+		console.log(random.hasClass("picX"))
+
 
 	} else {
-		random.addClass("active");
+		random.addClass("active").animate
+			({
+				opacity:1
+			});
 		console.log("running swap()")
-		time = Math.floor(Math.random() * 1000);
+		time = (Math.floor(Math.random() * 1000));
 		console.log(time)
 		random = $(boxes.get(Math.floor(Math.random() * boxes.length)));
-console.log(random.hasClass("picX"))
+		console.log(random.hasClass("picX"))
 	}
+
+		$(".active").click(function(){
+			console.log("Yay, click");
+			$(this).removeClass("active").animate
+			({
+			opacity:1
+			})
+			$(this).addClass("picX")
+		display++;
+		console.log(display)
+});
 }
-
-
 
 
